0c808581cc2892fcd74ac85db1823cdca2856b93
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-08-26 14:40:13 -0300

    Implementando autorizacao

--- src/main/java/com/mcexpress/config/SecurityConfig.java
+++ src/main/java/com/mcexpress/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,10 +20,12 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mcexpress.security.JWTAuthenticationFilter;
+import com.mcexpress.security.JWTAuthorizationFilter;
 import com.mcexpress.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		
 	@Autowired
@@ -34,10 +37,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JWTUtil jwtUtil;
 	
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/deposito/**", "/usuario/**", "/login/**" };
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**", "/login/**" };
 	
 	private static final String[] PUBLIC_MATCHERS_GET = { "/contribuintes/**", "/feriado/**",
-			"/funcionarios/**", "/historicorecibo/**", "/devolvido/**", "/recibos/**" };
+			"/funcionarios/**", "/historicorecibo/**", "/devolvido/**" };
+	
+//	private static final String[] PUBLIC_MATCHERS_POST = { "/usuario/**", "/deposito/**"};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -45,13 +50,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-			.antMatchers(PUBLIC_MATCHERS).permitAll()
-			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-			.anyRequest().authenticated();
-		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+			http.cors().and().csrf().disable();
+			http.authorizeRequests()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
+			http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+			http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+			http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 		
 	@Override
