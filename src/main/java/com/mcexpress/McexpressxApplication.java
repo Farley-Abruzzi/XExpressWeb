827917dbd6a21d404448f09100182c6e055f1742
Farley Abruzzi <farleyabruzzi@gmail.com>
2020-09-29 12:02:36 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/mcexpress/McexpressxApplication.java
+++ src/main/java/com/mcexpress/McexpressxApplication.java
@@ -1,14 +1,20 @@
 package com.mcexpress;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mcexpress.services.S3Service;
 
 
 @SpringBootApplication // Essa implementação executa a instanciação no momento em que a aplicação
 						// iniciar
 public class McexpressxApplication implements CommandLineRunner {
-
+	
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		
 		SpringApplication.run(McexpressxApplication.class, args);
@@ -16,8 +22,7 @@ public class McexpressxApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// Aqui vou instanciar os meus objetos
-
+		s3Service.uploadFile("C:\\Users\\eliseu\\Pictures\\farley.jpg");
 	}
 	
 
